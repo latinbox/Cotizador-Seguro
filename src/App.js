8f8e9545ec4b423bd313ef61db186e829dd2f751
Camilo <camilo.gonzalez@bailacthor.com>
2021-04-26 13:14:54 -0400

    styledcomponents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import Header from './componentes/Header';
+import styled from '@emotion/styled';
+import Formulario from './componentes/Formulario';
+const Contenedor = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+`;
+const ContenedorFormulario = styled.div`
+    background-color: #FFF;
+    padding: 3rem;
+`;
 function App() {
   return (  
-    <Header
-    titulo='Cotizador de seguros'
-     />
+    <Contenedor>
+      <Header
+        titulo='Cotizador de seguros'
+      />
+      <ContenedorFormulario>
+      <Formulario />
+      </ContenedorFormulario>
+    </Contenedor> 
   );
 }
  
